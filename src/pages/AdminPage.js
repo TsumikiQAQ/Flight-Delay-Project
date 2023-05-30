ccdc5a73f041a6220a794d78f52ba5c637476ffd
Zacharyii <1903253513@qq.com>
2023-05-30 21:50:11 +0800

    adminPageExchange

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,11 +1,11 @@
-import { useContractFunction} from "@usedapp/core";
+import { useContractFunction } from "@usedapp/core";
 import { Web3Provider } from "@ethersproject/providers";
 import { utils } from 'ethers';
 import { useRef, useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const AdminPage = ({contract}) => {
+const AdminPage = ({ contract }) => {
     //  /**
     //  * @dev 添加航空公司
     //  * @param _airlineCompany 航空公司的地址
@@ -17,19 +17,14 @@ const AdminPage = ({contract}) => {
     //   * @param _airlineCompany 航空公司的地址
     //   */
     //  function removeAirlines(address _airlineCompany) external;
-    
- console.log(contract);
+
+    console.log(contract);
     const delayRates = useRef();
     const cancelRates = useRef();
     const airlineAddress = useRef();
     const removeAirlineAddress = useRef();
     const airlineName = useRef();
     const removeairlineName = useRef();
-    const divElement = document.querySelector('.bg-blue-100');
-divElement.style.display = 'flex';
-divElement.style.justifyContent = 'center';
-divElement.style.alignItems = 'center';
-
 
     // const contractInterface = new utils.Interface(ALContractABI.abi);
     // const contractAddress ='0x620E06BCD8437dee974f1d8e4cE33AeC73A8563f';
@@ -38,100 +33,114 @@ divElement.style.alignItems = 'center';
     // const contract = new Contract(contractAddress,contractInterface,signer);
     // const contract = new Contract(contractAddress,contractInterface)
     const [functionName, setFunctionName] = useState('');
-    const {state, send} = useContractFunction(contract, functionName);
+    const { state, send } = useContractFunction(contract, functionName);
 
-    const addAirlines = () =>{
+    function CenteredDiv(props) {
+        const divStyle = {
+            display: 'bg-blue-100',
+            margin: '0 auto',
+        };
+
+        return (
+            <div style={divStyle}>
+                {props.children}
+            </div>
+        );
+    }
+
+    const addAirlines = () => {
         const address = airlineAddress.current.value;
-        if(!utils.isAddress(address)){
+        if (!utils.isAddress(address)) {
             console.log(address);
             toast.error("请输入一个正确的地址值!");
-        } else if(!airlineName.current.value) {
+        } else if (!airlineName.current.value) {
             toast.error("航空公司名称不能为空!")
-        }else if(!delayRates.current.value || delayRates.current.value<0 || delayRates.current.value>=100) {
+        } else if (!delayRates.current.value || delayRates.current.value < 0 || delayRates.current.value >= 100) {
             toast.error("延误率不能为空且在0-100之间!")
-        }else if(!cancelRates.current.value || cancelRates.current.value<0 || cancelRates.current.value>=100 ) {
+        } else if (!cancelRates.current.value || cancelRates.current.value < 0 || cancelRates.current.value >= 100) {
             toast.error("取消率不能为空且在0-100之间!")
-        }else{
-           setFunctionName('addAirlines')
+        } else {
+            setFunctionName('addAirlines')
         }
     }
-    const removeAirlines = () =>{
-        if(!utils.isAddress(removeAirlineAddress.current.value)){
+    const removeAirlines = () => {
+        if (!utils.isAddress(removeAirlineAddress.current.value)) {
             toast.error("请输入一个正确的地址值!");
-        } else if(!removeairlineName.current.value) {
+        } else if (!removeairlineName.current.value) {
             toast.error("航空公司名称不能为空!")
-        }else{
+        } else {
             setFunctionName('removeAirlines')
         }
     }
 
 
     useEffect(() => {
-        if(functionName === 'addAirlines'){
-            send(airlineName.current.value,airlineAddress.current.value,delayRates.current.value*100,cancelRates.current.value*100);
+        if (functionName === 'addAirlines') {
+            send(airlineName.current.value, airlineAddress.current.value, delayRates.current.value * 100, cancelRates.current.value * 100);
             setFunctionName('');
-        } else if(functionName === 'removeAirlines'){
+        } else if (functionName === 'removeAirlines') {
             send(removeAirlineAddress.current.value);
             setFunctionName('');
         }
     }, [functionName, send])
 
     useEffect(() => {
-        if(state.errorMessage){
+        if (state.errorMessage) {
             toast.error(state.errorMessage);
         }
     }, [state])
     return (
-        <div className="grid grid-cols-5 gap-4 mt-5 mx-10">
+        <div className="grid grid-cols-5 gap-4 mt-5 mx-10 ">
             {/* 注册航空公司，将航空公司地址添加到航空公司dao */}
-            {/* <div style={{ display: "flex", justifyContent: "center" }}></div> */}
-            <div className="bg-blue-100 p-8 rounded-3xl filter drop-shadow-xl col-span-3 ">
+            {/* <CenteredDiv> */}
+            <div className="bg-blue-100 p-8 rounded-3xl filter drop-shadow-xl col-span-5" style={{ width: '900px', margin: '0 auto' }}>
                 <h1 className="text-center font-bold text-3xl blue-text">注册航空公司</h1>
                 {/* 输入航空公司名字 */}
                 <div className="mt-5">
-                    <label htmlFor="name" className="text-xl mr-5 inline-block text-right w-1/5 blue-text">航空公司名字</label>
-                    <input ref={airlineName} name="name" type="text" className="text-black border w-3/5 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3"/>
+                    <label htmlFor="name" className="text-xl mr-5 inline-block text-right w-1/5 blue-text">航空公司名称</label>
+                    <input ref={airlineName} name="name" type="text" className="text-black border w-2/3 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3" placeholder="请输入您的航空公司名称" />
                 </div>
                 {/* 输入航空公司地址 */}
                 <div className="mt-5">
                     <label htmlFor="address" className="text-xl mr-5 inline-block text-right w-1/5 blue-text">航空公司地址</label>
-                    <input ref={airlineAddress} name="address" type="text" className="text-black border w-3/5 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3"/>
+                    <input ref={airlineAddress} name="address" type="text" className="text-black border w-2/3 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3" placeholder="请输入您的航空公司地址" />
                 </div>
                 <div className="mt-5">
                     <label htmlFor="delayRates" className="text-xl mr-5 inline-block text-right w-1/5 blue-text">航空公司延误率</label>
-                    <input ref={delayRates} name="delayRates" type="number" className="text-black border w-3/5 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3"/>
-                </div> 
+                    <input ref={delayRates} name="delayRates" type="number" className="text-black border w-2/3 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3" placeholder="请输入您的航空公司延误率" />
+                </div>
                 <div className="mt-5">
                     <label htmlFor="cancelRates" className="text-xl mr-5 inline-block text-right w-1/5 blue-text">航空公司取消率</label>
-                    <input ref={cancelRates} name="cancelRates" type="number" className="text-black border w-3/5 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3"/>
-                </div>   
+                    <input ref={cancelRates} name="cancelRates" type="number" className="text-black border w-2/3 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3" placeholder="请输入您的航空公司取消率" />
+                </div>
                 {/* 将航空公司添加进dao名单 */}
                 <div className="mt-10 text-center">
                     <button onClick={addAirlines} className="py-2 px-3 bg-red-700 rounded-md text-white text-lg hover:bg-red-400">注册</button>
                 </div>
             </div>
+            {/* </CenteredDiv> */}
 
-            
             {/*注销航空公司，将航空公司移除dao组织*/}
-            {/* <div style={{ display: "flex", justifyContent: "center" }}></div> */}
-            <div className="bg-blue-100 p-8 rounded-3xl filter drop-shadow-xl col-span-3" >
+            {/* <CenteredDiv> */}
+            <div className="bg-blue-100 p-8 rounded-3xl filter drop-shadow-xl col-span-5" style={{ width: '900px', margin: '0 auto' }}>
                 <h1 className="blue-text text-center font-bold text-3xl">注销航空公司</h1>
                 {/* 输入航空公司名字 */}
                 <div className="mt-5">
                     <label htmlFor="name" className="blue-text text-xl mr-5 inline-block text-right w-1/5">航空公司名字</label>
-                    <input ref={removeairlineName} name="name" type="text" className="text-black border w-3/5 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3"/>
+                    <input ref={removeairlineName} name="name" type="text" className="text-black border w-2/3 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3" placeholder="请输入您要注销的航空公司名称" />
                 </div>
                 {/* 输入航空公司地址 */}
                 <div className="mt-5">
                     <label htmlFor="address" className="blue-text text-xl mr-5 inline-block text-right w-1/5">航空公司地址</label>
-                    <input ref={removeAirlineAddress} name="address" type="text" className="text-black border w-3/5 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3"/>
-                </div>  
+                    <input ref={removeAirlineAddress} name="address" type="text" className="text-black border w-2/3 rounded-xl focus:outline-none focus:border-indigo-500 mt-5 py-1 px-3" placeholder="请输入您要注销的航空公司地址" />
+                </div>
                 {/* 将航空公司添加进dao名单 */}
                 <div className="mt-10 text-center">
                     <button onClick={removeAirlines} className="py-2 px-3 bg-red-700 rounded-md text-white text-lg hover:bg-red-400">注销</button>
                 </div>
-                
             </div>
+            {/* </CenteredDiv> */}
+
             <ToastContainer
                 position="bottom-right"
                 autoClose={5000}
@@ -142,7 +151,7 @@ divElement.style.alignItems = 'center';
                 pauseOnFocusLoss
                 draggable
                 pauseOnHover
-                />
+            />
         </div>
     );
 }
